793e5a33212d5dfa0fcad0517bac2967c1825b0c
EricRiney <fails.to.install@gmail.com>
2017-05-09 13:01:33 -0700

    b

--- js/grayscale.js
+++ js/grayscale.js
@@ -45,7 +45,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 11,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(47.6062, 122.3321), // Seattle
